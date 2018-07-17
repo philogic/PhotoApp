d142e22089b6512de1db588a0131d3315f79fd80
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-07-17 16:06:41 +0200

    Install and implement redux-logger. It helps tracking state changing process.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {View} from 'react-native';
 import {Provider} from "react-redux";
 import {createStore, applyMiddleware} from "redux";
+import logger from "redux-logger";
 import ReduxThunk from "redux-thunk";
 import Header from "./src/components/header";
 import PhotosFetch from "./src/components/photosFetch";
@@ -9,7 +10,7 @@ import reducers from "./src/reducers";
 
 export default class App extends Component {
   render() {
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk, logger));
     return (
       <Provider store={store}>
         <View>
